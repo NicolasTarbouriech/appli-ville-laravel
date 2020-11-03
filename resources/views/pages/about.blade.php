16697afd5f8f4d2fa5c9b8d60dcf60bca5a643ee
Nicolas Tarbouriech <65289313+NicolasTarbouriech@users.noreply.github.com>
2020-11-03 16:03:50 +0100

    appli fini

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,10 +5,12 @@
 
 @section('content')
 
-        <img src="/image/Teacher-du-net-logo-1.png" alt="Logo des Teachers du Net">
+        <img src="{{ asset('/image/Teacher-du-net-logo-1.png')}}" height="150" width="200" 
+        alt="Logo des Teachers du Net" class="my-12 rounded shadow-md">
 
-        <p>Conçu avec &hearts; par Nicolas Tarbouriech</p>
+        <h2 class="mb-5 text-gray-700">Conçu avec <span class="text-pink-500">&hearts;</span> par Nicolas Tarbouriech</h2>
 
-        <p><a href="{{ route('home') }} ">Revenir à la page d'accueil</a></p>
+        <p><a href="{{ route('home') }} " class="text-indigo-500 hover:text-indigo-600
+                underline" >Revenir à la page d'accueil</a></p>
     
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,9 +4,11 @@
 
 
 @section('content')
-    <img src="/image/drapeau-france.jpg" alt="Drapeau Français">
-    <h1>Bonjour de Montpellier</h1>
+    <img src="{{ asset('/image/flag-France.png')}}" height="100" width="200" alt="Drapeau Français"
+     class="mt-12 rounded shadow h-32">
     
-    <p>Il est {{ date('h:i A') }}.</p>
+    <h1 class="mt-5 text-3xl sm:text-5xl font-semibold text-indigo-600">Bonjour de Montpellier</h1>
+    
+    <p class="text-lg text-gray-800">Il est {{ date('h:i A') }}.</p>
 @endsection
 
